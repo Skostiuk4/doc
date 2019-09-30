9d16ba3d50f56af5679d28400b5a0416afbc66ca
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-30 16:15:37 +0300

    Added funtion

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
 <?php
 
 function file_get_content($path) {
-	$filename = ("text.txt"); 
 	if (!file_exists ($path)) {
 	 echo  ("File not found");
 	}
-	
 	$f = fopen($path,'r');
 	$length = filesize("text.txt");
 	$str = fread($f, $length);
 	fclose($f);
-	
-	
 	return $str;
-  
  }
-
  echo file_get_content("text.txt");
  
 ?>
