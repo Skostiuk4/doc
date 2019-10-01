824639c9febf793de1c048561fee77a52c44ef4f
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-01 13:54:11 +0300

    Add function

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	function file_get_content($path){
+function file_get_content($path){
 	  if (!file_exists($path)) {
 	    return ("File not found!s");
 	  } else {
@@ -10,4 +10,22 @@
 
 	echo file_get_content("4.txt");
 
-?>
+
+
+	function file_get ($files) {
+		$files = array ('text1.txt','order.txt','sample.txt','demo.txt');
+		foreach ($files as $file) {
+			$content = file_get_contents($files);
+
+		if (file_exists($files)) {
+			return ($content);
+		}	else {
+			return  ($content);
+		}
+
+
+		}
+	}
+	echo file_get ("text1.txt");
+
+
