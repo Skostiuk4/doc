c8e82579ebbab12f1a9e1bcafe6a7b7ecdce1837
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-01 10:04:53 +0300

    final commit

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
-function file_get_content($path) {
-	if (!file_exists ($path)) {
-	return  ("File not found");
+	function file_get_content($path){
+	  if (!file_exists($path)) {
+	    return ("File not found!s");
+	  } else {
+	    return file_get_contents($path);
+	  }
 	}
-	else {
-	return $f = file_get_contents("text.txt");
-	}
- }
- echo file_get_content("text.txt");
- 
+
+	echo file_get_content("4.txt");
+
 ?>
