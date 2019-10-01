80ae28c356c8d192963bbcdd30de90bfa1a190bd
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-01 15:50:31 +0300

    Edit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-function file_get_content($path){
+	function file_get_content($path){
 	  if (!file_exists($path)) {
 	    return ("File not found!s");
 	  } else {
@@ -14,18 +14,12 @@ function file_get_content($path){
 
 	function file_get ($files) {
 		$files = array ('text1.txt','order.txt','sample.txt','demo.txt');
-		foreach ($files as $file) {
-			$content = file_get_contents($files);
-
-		if (file_exists($files)) {
-			return ($content);
-		}	else {
-			return  ($content);
-		}
-
-
+		if (!file_exists($files)) {
+			return ("File not found!s");
+		} else {
+			return file_get_contents($files);
 		}
 	}
-	echo file_get ("text1.txt");
+	echo file_get_content("text1.txt","order.txt","sample.txt","demo.txt");
 
 
