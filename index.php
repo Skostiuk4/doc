639c631308195388263472c0e2a38fabf6e7000e
Skostiuk4 <Skostiuk4@gmail.com>
2019-10-02 12:16:32 +0300

    Add

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-	function file_get_content($path){
+function file_get_content($path){
 	  if (!file_exists($path)) {
 	    return ("File not found!s");
 	  } else {
@@ -12,14 +12,19 @@
 
 
 
-	function file_get ($files) {
-		$files = array ('text1.txt','order.txt','sample.txt','demo.txt');
-		if (!file_exists($files)) {
-			return ("File not found!s");
-		} else {
-			return file_get_contents($files);
-		}
+	function file_get ($files){
+    foreach ($files as $file) {
+      file_get_content($files);
+      $files = array("text1.txt", "order.txt", "sample.txt", "demo.txt");
+      
+      if (file_exists($files)) {
+        return ("File not found!");
+      } else {
+        return file_get_contents($files);
+
+      }
+    }
 	}
-	echo file_get_content("text1.txt","order.txt","sample.txt","demo.txt");
+  echo file_get($files);
 
 
